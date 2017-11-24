f2a421f31d565eb98c1e32c36c92dc8b0a1c298f
Administrator <Administrator@svn.richway.com>
2017-11-24 10:31:45 +0800

    20171124更新

---
+++ taotao-sso/src/main/java/com/taotao/sso/service/UserService.java
@@ -0,0 +1,8 @@
+package com.taotao.sso.service;
+
+import com.taotao.common.pojo.TaotaoResult;
+
+public interface UserService {
+	
+	TaotaoResult checkData(String content,Integer type);
+}
---
+++ taotao-sso/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.taotao.sso.service.impl;
+
+import java.util.List;
+
+import javax.sound.sampled.LineListener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.mapper.TbUserMapper;
+import com.taotao.pojo.TbUser;
+import com.taotao.pojo.TbUserExample;
+import com.taotao.pojo.TbUserExample.Criteria;
+import com.taotao.sso.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private TbUserMapper  userMapper;
+	
+	@Override
+	public TaotaoResult checkData(String content, Integer type) {
+		
+		TbUserExample example=new TbUserExample();
+		Criteria criteria = example.createCriteria();
+		//校验数据
+		if(1==type){
+			criteria.andUsernameEqualTo(content);
+		}else if(2==type){
+			criteria.andPhoneEqualTo(content);
+		}else{
+			criteria.andEmailEqualTo(content);
+		}
+		//执行查询
+		List<TbUser> list = userMapper.selectByExample(example);
+		if(list==null||list.size()==0){
+			return TaotaoResult.ok(true);
+		}
+		return TaotaoResult.ok(false);
+	}
+
+}
