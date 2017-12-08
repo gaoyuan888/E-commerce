1d3adffb79e01b5400c8a22fa2688e17b123d33d
Administrator <Administrator@svn.richway.com>
2017-12-08 22:36:37 +0800

    20171208更新

---
+++ taotao-order/src/main/java/com/taotao/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,79 @@
+package com.taotao.order.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.mapper.TbOrderItemMapper;
+import com.taotao.mapper.TbOrderMapper;
+import com.taotao.mapper.TbOrderShippingMapper;
+import com.taotao.order.dao.JedisClient;
+import com.taotao.order.service.OrderService;
+import com.taotao.pojo.TbOrder;
+import com.taotao.pojo.TbOrderItem;
+import com.taotao.pojo.TbOrderShipping;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	@Autowired
+	private TbOrderMapper orderMapper;
+	
+	@Autowired
+	private TbOrderItemMapper orderItemMapper;
+	
+	@Autowired
+	private TbOrderShippingMapper orderShppingMapper;
+	
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${ORDER_GEN_KEY}")
+	private String ORDER_GEN_KEY;
+	
+	@Value("${ORDER_INIT_ID}")
+	private String ORDER_INIT_ID;
+	
+	@Value("${ORDER_DETAIL_GEN_KEY}")
+	private String ORDER_DETAIL_GEN_KEY;
+	
+	@Override
+	public TaotaoResult createOrder(TbOrder order, List<TbOrderItem> itemList, TbOrderShipping orderShipping) {
+		String string = jedisClient.get(ORDER_GEN_KEY);
+		if(StringUtils.isBlank(string)){
+			jedisClient.set(ORDER_GEN_KEY, ORDER_INIT_ID);
+		}
+		long orderId = jedisClient.incr(ORDER_GEN_KEY);
+		
+		order.setOrderId(orderId+"");
+//		'状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭',
+		order.setStatus(1);
+		Date date=new Date();
+		order.setCreateTime(date);
+		order.setUpdateTime(date);
+//		1.已评价；2未评价
+		order.setBuyerRate(0);
+		//向订单插入数据
+		orderMapper.insert(order);
+		//插入订单明细
+		for (TbOrderItem tbOrderItem : itemList) {
+			//补全订单明细
+			long orderDetilId = jedisClient.incr(ORDER_DETAIL_GEN_KEY);
+			tbOrderItem.setId(orderDetilId+"");
+			orderItemMapper.insert(tbOrderItem);
+		}
+		//插入物流表
+		orderShipping.setOrderId(orderId+"");
+		orderShipping.setCreated(date);
+		orderShipping.setUpdated(date);
+		orderShppingMapper.insert(orderShipping);
+		return TaotaoResult.ok();
+	}
+
+
+}
